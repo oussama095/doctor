e0b4fd509d18ce20510295a51fbfcf6b8452a472
oussama095 <jesuisnull95@gmail.com>
2021-05-10 04:56:16 +0200

    implement upper bar && notification

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,41 @@
-import { NgModule } from '@angular/core';
-import { BrowserModule } from '@angular/platform-browser';
+import {NgModule} from '@angular/core';
+import {BrowserModule} from '@angular/platform-browser';
 
-import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
+import {AppRoutingModule} from './app-routing.module';
+import {AppComponent} from './app.component';
+import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
+import {ToolbarComponent} from './shared/component/toolbar/toolbar.component';
+import {MatToolbarModule} from '@angular/material/toolbar';
+import {MatIconModule} from '@angular/material/icon';
+import {MatButtonModule} from '@angular/material/button';
+import {MatMenuModule} from '@angular/material/menu';
+import {MatBadgeModule} from '@angular/material/badge';
+import {MatDialogModule} from '@angular/material/dialog';
+import {NotificationDialogComponent} from './shared/component/notification-dialog/notification-dialog.component';
+import {HTTP_INTERCEPTORS, HttpClientModule} from '@angular/common/http';
+import {Interceptor} from './shared/service/interceptor/interceptor';
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ToolbarComponent,
+    NotificationDialogComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    HttpClientModule,
+    MatToolbarModule,
+    MatIconModule,
+    MatMenuModule,
+    MatBadgeModule,
+    MatButtonModule,
+    MatDialogModule
   ],
-  providers: [],
+  providers: [{provide: HTTP_INTERCEPTORS, useClass: Interceptor, multi: true}],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {
+}
---
+++ src/app/shared/service/notification/notificationService.ts
@@ -0,0 +1,21 @@
+import {Injectable} from '@angular/core';
+import {HttpClient} from '@angular/common/http';
+import {Notification} from '../../schema/notification';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class NotificationService {
+  constructor(private http: HttpClient) {
+  }
+
+  getAllNotifications(): Promise<Notification[]> {
+    const reqUrl = 'notifications';
+    return this.http.get<Notification[]>(reqUrl).toPromise().then((notification) => notification);
+  }
+
+  setReadNotification(id: string): Promise<Notification> {
+    const reqUrl = `notifications/notification/${id}`;
+    return this.http.put<Notification>(reqUrl, null).toPromise().then((notification) => notification);
+  }
+}
