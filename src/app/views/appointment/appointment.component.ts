bd7a3ed408ad35e38a558fc72dcc6e58822dca98
oussama095 <jesuisnull95@gmail.com>
2021-05-11 06:36:37 +0200

    add fullCalender

--- src/app/views/appointment/appointment.component.ts
+++ src/app/views/appointment/appointment.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {CalendarOptions} from '@fullcalendar/angular';
 
 @Component({
   selector: 'app-appointment',
@@ -7,9 +8,26 @@ import { Component, OnInit } from '@angular/core';
 })
 export class AppointmentComponent implements OnInit {
 
-  constructor() { }
+  // @ts-ignore
+  calendarOptions: CalendarOptions = {
+    initialView: 'timeGridWeek',
+    dateClick: this.handleDateClick.bind(this), // bind is important!
+    events: [
+      {title: 'event 1', date: '2021-05-01'},
+      {title: 'event 2', date: '2021-05-02'}
+    ]
+  };
+
+
+  constructor() {
+  }
 
   ngOnInit(): void {
+  }
+
+  // tslint:disable-next-line:typedef
+  handleDateClick(arg: { dateStr: string; }) {
+    alert('date click! ' + arg.dateStr);
   }
 
 }
--- src/app/views/appointment/appointment.module.ts
+++ src/app/views/appointment/appointment.module.ts
@@ -1,12 +1,21 @@
 import {NgModule} from '@angular/core';
 import {CommonModule} from '@angular/common';
 import {AppointmentComponent} from './appointment.component';
+import {FullCalendarModule} from '@fullcalendar/angular'; // the main connector. must go first
+import dayGridPlugin from '@fullcalendar/daygrid'; // a plugin
+import interactionPlugin from '@fullcalendar/interaction'; // a plugin
 
+
+FullCalendarModule.registerPlugins([ // register FullCalendar plugins
+  dayGridPlugin,
+  interactionPlugin
+]);
 
 @NgModule({
   declarations: [AppointmentComponent],
   imports: [
-    CommonModule
+    CommonModule,
+    FullCalendarModule
   ],
   exports: [AppointmentComponent]
 })
