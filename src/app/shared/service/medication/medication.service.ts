cd536d1c4ceb2ffc4681533cf546cceb54ab52f5
oussama095 <jesuisnull95@gmail.com>
2021-05-16 18:30:37 +0200

    add medication overview

--- src/app/shared/service/medication/medication.service.ts
+++ src/app/shared/service/medication/medication.service.ts
@@ -1,9 +1,18 @@
-import { Injectable } from '@angular/core';
+import {Injectable} from '@angular/core';
+import {HttpClient} from '@angular/common/http';
+import {Medication} from '../../model/medication';
 
 @Injectable({
   providedIn: 'root'
 })
 export class MedicationService {
 
-  constructor() { }
+  private baseUrl = 'medication';
+
+  constructor(private http: HttpClient) {
+  }
+
+  getPatientAllMedication(patientId: string): Promise<Medication[]> {
+    return this.http.get<Medication[]>(this.baseUrl + '/' + patientId).toPromise().then((medications) => medications);
+  }
 }
